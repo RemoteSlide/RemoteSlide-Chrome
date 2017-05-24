bcf10a6477fde43441b8229d6cd10000bad49c80
inventivetalent <inventivetalent@inventivegames.de>
2017-05-25 01:08:10 +0200

    make sure the message contains session data

--- sessionGrabber.js
+++ sessionGrabber.js
@@ -1,22 +1,24 @@
 console.log("Waiting for session message...")
 chrome.runtime.onMessageExternal.addListener(function (msg, sender, sendResponse) {
-    console.log("Session msg: " + JSON.stringify(msg))
-    chrome.storage.local.get(["session"], function (items) {
-        var oldSession = items.session;
+    if (msg.session) {
+        console.log("Session msg: " + JSON.stringify(msg))
+        chrome.storage.local.get(["session"], function (items) {
+            var oldSession = items.session;
 
-        msg.session.sessionTime = new Date().valueOf();
-        chrome.storage.local.set({"session": msg.session}, function () {
-            if (!oldSession || oldSession.session != msg.session.session) {
-                chrome.notifications.create('new-session-created-notification', {
-                    type: 'basic',
-                    iconUrl: 'logo-128.png',
-                    title: 'Session Updated',
-                    message: 'A new session has been created\n' +
-                    'Click the icon to start presenting!',
-                    contextMessage: msg.session.session
-                }, function (notificationId) {
-                });
-            }
+            msg.session.sessionTime = new Date().valueOf();
+            chrome.storage.local.set({"session": msg.session}, function () {
+                if (!oldSession || oldSession.session != msg.session.session) {
+                    chrome.notifications.create('new-session-created-notification', {
+                        type: 'basic',
+                        iconUrl: 'logo-128.png',
+                        title: 'Session Updated',
+                        message: 'A new session has been created\n' +
+                        'Click the icon to start presenting!',
+                        contextMessage: msg.session.session
+                    }, function (notificationId) {
+                    });
+                }
+            });
         });
-    });
+    }
 });
