46ad8b6ba932da96f4e1894868d974ef68c45e6c
inventivetalent <inventivetalent@inventivegames.de>
2017-05-25 21:43:27 +0200

    stuff

--- background.js
+++ background.js
@@ -21,4 +21,31 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
             }
         }
     }
+    if (msg.action == "sessionUpdate") {
+        chrome.storage.local.get(["session"], function (items) {
+            console.log(items)
+            if (items && items.session) {
+                $.extend(msg.session, items.session);
+            }
+            chrome.storage.local.set({"session": msg.session}, function () {
+            });
+        });
+    }
+    if (msg.action == 'controlUpdate') {
+        if (msg.active) {
+            chrome.browserAction.setBadgeBackgroundColor({color: "lime"})
+        } else {
+            chrome.browserAction.setBadgeBackgroundColor({color: "blue"})
+            chrome.browserAction.setBadgeText({text: ""})
+        }
+    }
+})
+
+chrome.storage.onChanged.addListener(function (changes, area) {
+    if (area == 'local') {
+        console.log("changes: " + JSON.stringify(changes))
+        if (changes.session) {
+
+        }
+    }
 })
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 var app = angular.module("app", []);
 
-app.controller("mainCtrl", function ($scope, $timeout, $http) {
+app.controller("mainCtrl", function ($scope, $timeout) {
+    $scope.controlActive = false;
     $scope.session = {
         session: "",
         qr: "",
         bookmarkContent: "",
-        initialized: false,
         refresh: function () {
             chrome.storage.local.get(["session"], function (items) {
                 console.log(items)
                 $timeout(function () {
                     if (items.session && items.session.sessionTime && (new Date().valueOf() - items.session.sessionTime < 3.6e+6)) {
-                        $scope.session.session = items.session.session;
-                        $scope.session.qr = items.session.qr;
+                        $.extend($scope.session, items.session);
                     } else {
                         // session already expired
                         chrome.tabs.create({url: "https://remote-sli.de"});
                     }
                 })
             });
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {action: "state_request"}, function (response) {
-                });
+
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {action:"stateRequest"});
             });
+
+            console.info($scope.session)
         },
         injectBookmarkScript: function () {
             // $("body").append("<script src='https://remote-sli.de/res/host-bookmark.js'></script>")
@@ -32,30 +33,53 @@ app.controller("mainCtrl", function ($scope, $timeout, $http) {
             //     chrome.tabs.executeScript(null, {code: data})
             // });
 
-            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-                chrome.tabs.executeScript(tabs[0].id, {
+            chrome.tabs.getSelected(null, function (tab) {
+                chrome.tabs.executeScript(tab.id, {
                     file: "lib/jquery.min.js"
                 });
-                chrome.tabs.executeScript(tabs[0].id, {
+                chrome.tabs.executeScript(tab.id, {
                     file: "lib/socket.io.js"
                 });
 
-                chrome.tabs.executeScript(tabs[0].id, {
+                chrome.tabs.executeScript(tab.id, {
                     code: "remote_slide = { session: '" + $scope.session.session + "' }"
                 });
-                chrome.tabs.executeScript(tabs[0].id, {
+                chrome.tabs.executeScript(tab.id, {
                     file: "inject/res/pageController.js"
                 });
-                chrome.tabs.sendMessage(tabs[0].id, {action: "inject_controller"}, function (response) {
+                chrome.tabs.sendMessage(tab.id, {action: "inject_controller"}, function (response) {
+                });
+
+                chrome.storage.local.set({"controlledTab": tab.id}, function () {
                 });
             });
+
         }
     }
+    chrome.storage.onChanged.addListener(function (changes, area) {
+        if (area == 'local') {
+            console.log("changes: " + JSON.stringify(changes))
+            if (changes.session) {
+                if (changes.session.newValue) {
+                    $.extend($scope.session, changes.session.newValue);
+                } else {
+                    $scope.session.session = "";
+                }
+            }
+        }
+    })
 
     chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
-        if (msg.action == 'session_initialized') {
+        console.log(msg)
+        if (msg.action == 'controlUpdate') {
             $timeout(function () {
-                $scope.session.initialized = true;
+                $scope.controlActive = msg.active;
+
+                if(msg.active){
+                    chrome.browserAction.setBadgeBackgroundColor({color: "lime"})
+                }else{
+                    chrome.browserAction.setBadgeBackgroundColor({color: "blue"})
+                }
             })
         }
     });
