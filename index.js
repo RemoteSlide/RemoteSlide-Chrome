3aa596cc026c8992797a5095608b5699634709d3
inventivetalent <inventivetalent@inventivegames.de>
2017-05-26 15:28:47 +0200

    send the injector type

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.controller("mainCtrl", function ($scope, $timeout) {
                 })
             });
 
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, {action:"stateRequest"});
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, {action: "stateRequest"});
             });
 
             console.info($scope.session)
@@ -42,7 +42,10 @@ app.controller("mainCtrl", function ($scope, $timeout) {
                 });
 
                 chrome.tabs.executeScript(tab.id, {
-                    code: "remote_slide = { session: '" + $scope.session.session + "' }"
+                    code: "remote_slide = " + JSON.stringify({
+                        session: $scope.session.session,
+                        injector: 'extension_chrome'
+                    })
                 });
                 chrome.tabs.executeScript(tab.id, {
                     file: "inject/res/pageController.js"
@@ -75,9 +78,9 @@ app.controller("mainCtrl", function ($scope, $timeout) {
             $timeout(function () {
                 $scope.controlActive = msg.active;
 
-                if(msg.active){
+                if (msg.active) {
                     chrome.browserAction.setBadgeBackgroundColor({color: "lime"})
-                }else{
+                } else {
                     chrome.browserAction.setBadgeBackgroundColor({color: "blue"})
                 }
             })
